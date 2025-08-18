6b241791d3d2a1a05084fe9c4bd5939a3c53060e
zhucj <zhucj1028@163.com>
2025-08-18 22:23:00 +0800

    first commit

---
+++ src/main/java/com/fishpond/fishpondapp/FishpondAppApplication.java
@@ -0,0 +1,13 @@
+package com.fishpond.fishpondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishpondAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishpondAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fishpond/fishpondapp/FishpondAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishpond.fishpondapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishpondAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
